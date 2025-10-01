afc3162b945a6e4218561d9584f5a4d805b817e9
rjeff-dev <jeff_xd00@hotmail.com>
2025-10-01 00:23:25 -0300

    finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,21 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
-// Compilar SCSS
-function styles() {
+function styles(){
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
-// Otimizar imagens
-function images() {
-    return gulp.src('./src/images/*')
+function images(){
+    console.log("Rodando imagemin...");
+    return gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
-// Copiar HTML
-function html() {
-    return gulp.src('./index.html')
-        .pipe(gulp.dest('./dist'));
-}
+exports.default = gulp.parallel(styles, images);
 
-exports.default = gulp.parallel(styles, images, html);
-
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/images/*', gulp.parallel(images));
-    gulp.watch('./index.html', gulp.parallel(html));
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
