9daafe88b4cba06409d63abfd02842e406191a1c
rjeff-dev <jeff_xd00@hotmail.com>
2025-09-30 23:46:59 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,30 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
-function styles(){
+// Compilar SCSS
+function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 }
 
-function images(){
-    console.log("Rodando imagemin...");
-    return gulp.src('./src/images/**/*')
+// Otimizar imagens
+function images() {
+    return gulp.src('./src/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+// Copiar HTML
+function html() {
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+}
 
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+exports.default = gulp.parallel(styles, images, html);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/*', gulp.parallel(images));
+    gulp.watch('./index.html', gulp.parallel(html));
 }
