a00dc2d031af7ed2b19fcf9121abae0dcd283b9f
rjeff-dev <jeff_xd00@hotmail.com>
2025-10-03 00:57:07 -0300

    site

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,29 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
+// compila SCSS -> CSS
 function styles(){
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
+// otimiza imagens
 function images(){
-    console.log("Rodando imagemin...");
-    return gulp.src('./src/images/**/*')
+    return gulp.src('./images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+// copia HTML para dist
+function html(){
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+}
+
+exports.default = gulp.parallel(styles, images, html);
 
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./index.html', gulp.parallel(html));
 }
